1717b3d6a38fa281db588b02ebf2ec29f3774a0d
tiaratia <143604612+tiaratia@users.noreply.github.com>
2024-03-27 18:42:15 +0700

    Add files via upload

---
+++ contoh menggunakan variabel sederhana .py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+# operasi dan menipulasi string 
+# 1 menyambung string (concatenate)
+nama_pertama = "diana"
+nama_tengah = "F"
+nama_akhir = "putri"
+nama_lengkap = nama_pertama +" "+nama_tengah+"'" + nama_akhir
+print (nama_lengkap)
+
+
+# In[ ]:
+
+
+
+
