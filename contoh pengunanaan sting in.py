f0a4ed75dfa06509b51907cf65587889478db158
tiaratia <143604612+tiaratia@users.noreply.github.com>
2024-03-27 19:29:17 +0700

    Add files via upload

---
+++ contoh pengunanaan sting in.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# contoh in hasilnya true
+kalimat = "indonesia jaya"
+data = "jaya"
+print(data in kalimat)
+
+
+#contoh in hasilnyha false
+kalimat = "indonesia jaya"
+data = "hebat"
+print (data in kalimat)
+
+
+# In[ ]:
+
+
+
+
